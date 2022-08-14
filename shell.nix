71d23175a0be44d481429715586208326c03a807
Ollenurb <brunello.mtt@gmail.com>
2022-08-14 11:19:35 +0200

    Added shell.nix, Added SOM

---
+++ shell.nix
@@ -0,0 +1,32 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  # import mach-nix
+  mach-nix = import (builtins.fetchGit {
+    url = "https://github.com/DavHau/mach-nix";
+    ref = "refs/tags/3.3.0";
+  }) {};
+
+  overridedPython = mach-nix.mkPython {
+    python = "python38";
+    requirements = ''
+      scikit-learn
+      matplotlib
+      pydotplus
+      graphviz
+      scipy
+      jupyterlab
+    '';
+  };
+in
+pkgs.mkShell {
+  buildInputs = [
+    overridedPython
+    pkgs.graphviz
+  ];
+ /* shellHook = '' */
+ /*   jupyter-notebook */
+ /* ''; */
+}
+
+
